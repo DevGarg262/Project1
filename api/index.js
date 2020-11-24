4ea21445a6c5c18c5e7c744abc86ba3cd642ee65
Dev Garg <dgarg5908@gmail.com>
2020-11-24 13:20:13 +0530

    new code

---
+++ api/index.js
@@ -0,0 +1,76 @@
+// const { chromium } = require('playwright')
+// // const jsdom = require("jsdom");
+// // const { JSDOM } = jsdom;
+
+
+
+// const render = async (url, path) => {
+//   var browser = null
+//   // var url = "https://certificate-rho.vercel.app/svgTemplate.html?name=Dev Garg"
+//   try {
+//     browser = await chromium.launch({
+//       args: [
+//         '--no-sandbox',
+//         '--ignore-certificate-errors'
+//       ],
+//       dumpio: true
+//     })
+
+//     const page = await browser.newPage()
+
+//     await page.goto(url)
+//     await page.emulateMedia('screen')
+//     await page.pdf({
+//       path: "path.pdf",
+//       format: 'A4',
+//       displayHeaderFooter: true,
+//       printBackground: true,
+//       margin: {
+//         top: '120px',
+//         bottom: '50px',
+//         right: '0px',
+//         left: '0px'
+//       }
+//     })
+
+//     await browser.close()
+//     process.exit(0)
+//     // return;
+//   } catch (error) {
+//     console.log(error)
+//     await browser.close()
+//     process.exit(1)
+//   }
+// }
+
+// var myArgs = process.argv.slice(2)
+
+// render(myArgs[0], myArgs[1])
+
+// module.exports = async (req, res) => {
+//   const  { name } = req.query;
+  
+//   try {
+//     res.send({
+//       status: 200,
+//       // lyrics:
+//       //  await
+//       var a : 'https://certificate-rho.vercel.app/svgTemplate.html?name=${name}',
+//         render(,'./')
+//     })
+//   } catch (err) {
+//     res.send({
+//       status: 500,
+//       message: err.message,
+//     })
+//   }
+// }
+
+
+module.exports = (req, res) => {
+  const {
+    query: { name },
+  } = req
+
+  res.send(`https://project1-65dnl1uco.vercel.app/main.html?name=${name}`)
+}
--- api/name.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-    const {
-      query: { name },
-    } = req
-  
-    res.send(`https://project1-65dnl1uco.vercel.app/main.html?name=${name}`)
-  }
