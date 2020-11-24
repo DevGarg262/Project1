0fda44ce32334bf44470d87decc839d3d3443699
Dev Garg <dgarg5908@gmail.com>
2020-11-24 13:35:04 +0530

    pls

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-// const { chromium } = require('playwright')
+const { chromium } = require('playwright');
 // // const jsdom = require("jsdom");
 // // const { JSDOM } = jsdom;
 
@@ -72,5 +72,40 @@ module.exports = (req, res) => {
     query: { name },
   } = req
 
-  res.send(`https://project1-65dnl1uco.vercel.app/main.html?name=${name}`)
+  // res.send(`https://project1-65dnl1uco.vercel.app/main.html?name=${name}`)
+  res.send(async function render(){
+      try {
+    browser = await chromium.launch({
+      args: [
+        '--no-sandbox',
+        '--ignore-certificate-errors'
+      ],
+      dumpio: true
+    })
+
+    const page = await browser.newPage()
+    await page.goto(`https://project1-65dnl1uco.vercel.app/main.html?name=${name}`)
+    await page.emulateMedia('screen')
+    await page.pdf({
+      // path: "path.pdf",
+      format: 'A4',
+      displayHeaderFooter: true,
+      printBackground: true,
+      margin: {
+        top: '120px',
+        bottom: '50px',
+        right: '0px',
+        left: '0px',
+      }
+    })
+
+    await browser.close()
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    await browser.close()
+    process.exit(1)
+  }
 }
+  
+  )   }
