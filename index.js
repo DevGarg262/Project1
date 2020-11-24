2c8ceaac8b299cdc27db15d84a61b1bcd0bd67d5
Dev Garg <dgarg5908@gmail.com>
2020-11-24 12:01:44 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const { chromium } = require('playwright')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+
+
+const render = async (url, path) => {
+  var browser = null
+  var url = "https://certificate-rho.vercel.app/svgTemplate.html?name=Dev Garg"
+  try {
+    browser = await chromium.launch({
+      args: [
+        '--no-sandbox',
+        '--ignore-certificate-errors'
+      ],
+      dumpio: true
+    })
+
+    const page = await browser.newPage()
+
+    await page.goto(url)
+    await page.emulateMedia('screen')
+    await page.pdf({
+      path: "path.pdf",
+      format: 'A4',
+      displayHeaderFooter: true,
+      printBackground: true,
+      margin: {
+        top: '120px',
+        bottom: '50px',
+        right: '0px',
+        left: '0px'
+      }
+    })
+
+    await browser.close()
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    await browser.close()
+    process.exit(1)
+  }
+}
+
+var myArgs = process.argv.slice(2)
+
+render(myArgs[0], myArgs[1])
+
+
+
+
+
