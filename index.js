8604ee9b82b2fe49d7ef3ee2589097a05bbef838
Dev Garg <dgarg5908@gmail.com>
2020-11-24 13:15:24 +0530

    moved

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { chromium } = require('playwright')
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+// const jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
 
 
 
 const render = async (url, path) => {
   var browser = null
-  var url = "https://certificate-rho.vercel.app/svgTemplate.html?name=Dev Garg"
+  // var url = "https://certificate-rho.vercel.app/svgTemplate.html?name=Dev Garg"
   try {
     browser = await chromium.launch({
       args: [
@@ -35,6 +35,7 @@ const render = async (url, path) => {
 
     await browser.close()
     process.exit(0)
+    // return;
   } catch (error) {
     console.log(error)
     await browser.close()
@@ -46,7 +47,24 @@ var myArgs = process.argv.slice(2)
 
 render(myArgs[0], myArgs[1])
 
-
+module.exports = async (req, res) => {
+  const  { name } = req.query;
+  
+  try {
+    res.send({
+      status: 200,
+      // lyrics:
+      //  await
+      var a : 'https://certificate-rho.vercel.app/svgTemplate.html?name=${name}',
+        render(,'./')
+    })
+  } catch (err) {
+    res.send({
+      status: 500,
+      message: err.message,
+    })
+  }
+}
 
 
 
